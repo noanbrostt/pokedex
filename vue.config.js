f8ca97d00ff948d77601046052084f62601cc05d
Noan Brostt <calielnoan@gmail.com>
2023-10-09 21:33:20 -0300

    Config vuejs for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/pokedex/',
-  outputDir: 'src/app.vue'
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/pokedex/'
+  : '/'
+}
