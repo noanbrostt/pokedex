e7510d3b1d290c3e7f71748b0aac502a8b1ee4a9
Noan Brostt <calielnoan@gmail.com>
2023-10-21 14:18:40 -0300

    Background

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="">
+  <div>
     <img src="./assets/pokedex.png" alt="Pokedex.png">
-    <input class="input is-rounded" type="text" placeholder="Digite o nome do pokemon.." v-model="busca">
+    <input class="busca" type="text" placeholder="Digite o nome do pokemon.." v-model="busca">
     <div class="content">
       <PokeDiv v-for="(poke) in resultadoBusca" :key="poke.url" :name="poke.name" :url="poke.url"/>
     </div>
@@ -41,22 +41,35 @@ export default {
   },
   mounted() {
     // Define o título da página no momento em que o componente é montado
-    document.title = 'Pokédex by Noan Brostt';
+    document.title = 'Vue Pokédex by Noan';
   },
 }
 </script>
 
 <style>
+body {
+  min-height: 100vh;
+  background: url('./assets/background2.png');
+  background-size: 350px 350px;
+}
 #app {
   text-align: center;
 }
-#buscarBtn {
-  margin-top: 15px;
+.busca {
+  border-radius: 12px;
+  position: sticky;
+  top: 2px;
+  z-index: 3;
+  width: 94%;
+  padding: 8px 6px;
+  outline: none;
+  border: none;
+  box-shadow: 0px 1px 5px black;
 }
 .content {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
   grid-gap: 16px;
-  margin-inline: 30px;
+  margin-inline: 2%;
 }
 </style>
